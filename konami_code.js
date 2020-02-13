406768fde6691a8c68d67091637b425d3ad2c60f
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-13 08:54:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,17 @@ const codes = [
 ];
 
 function init() {
-
+  let count = 0;
+  document.body.addEventListener('keydown', (event) => {
+    if (event.key === codes[count]){
+      count += 1;
+    }
+    else{
+      count = 0;
+    }
+    if (count === codes.lenght){
+      alert('Konami!');
+      count = 0;
+    }
+  })
 }
