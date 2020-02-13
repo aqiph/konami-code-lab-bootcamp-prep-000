e4e26b3f9171c4be71e411f658e5b073a09d9ae7
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-13 09:21:08 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,16 +12,12 @@ const codes = [
 ];
 
 function init() {
-  let p = document.createElement('p');
-  p.innerText = 'Hellow World';
-  document.body.appendChild(p);
   let count = 0;
-  p.addEventListener('keydown', (event) => {
+  document.body.addEventListener('keydown', (event) => {
     if (event.key === codes[count]){
       count++;
     }
     else{
-      alert('Again!')
       count = 0;
     }
     if (count === codes.length){
